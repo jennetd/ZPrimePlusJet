0b3a23d407c48bd1dcc573318032f512ddaf5a4e
Martin <ka_hei_martin_kwok@brown.edu>
2019-01-29 09:56:08 -0600

    use 2016 V_SF

--- fitting/PbbJet/buildRhalphabetHbb.py
+++ fitting/PbbJet/buildRhalphabetHbb.py
@@ -31,7 +31,7 @@ SF2017={
             's_data'    : 8.701,            's_data_err': 0.433,
             's_mc'      : 8.027,            's_mc_err'  : 0.607,
             'BB_SF'     : 0.72,             'BB_SF_ERR' : 0.06, #2017 double-b SF
-            'V_SF'      : 1,                'V_SF_ERR'  : 0.01, #needs update
+            'V_SF'      : 0.993,            'V_SF_ERR'  : 0.043,
 }
 SF2016={
             'm_data'    : 82.657,           'm_data_err': 0.313,
