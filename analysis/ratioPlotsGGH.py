83a40ad668d9b1083ff3c625df4266c74bff7982
Javier Duarte <jduarte1@fnal.gov>
2019-02-12 15:05:11 -0600

    update ratio plots

--- analysis/ratioPlotsGGH.py
+++ analysis/ratioPlotsGGH.py
@@ -8,9 +8,10 @@ import sys
 import time
 import array
 import glob
-import os
+import os,json
 from plotHelpers import *
 from sampleContainer import *
+from normSampleContainer import *
 DBTMIN=-99
 #
 
@@ -49,13 +50,11 @@ def main(options,args,outputExists):
     #idir = "/eos/uscms/store/user/lpchbb/ggHsample_V11/sklim-v0-28Oct/"
     #odir = "plots_2016_10_31/"
     #idir = options.idir
-    idir = 'root://cmseos.fnal.gov//eos/uscms/store/user/lpcbacon/dazsle/zprimebits-v12.08-HWW/norm/'
-    idir_hadd = 'root://cmseos.fnal.gov//eos/uscms/store/user/lpcbacon/dazsle/zprimebits-v12.08-HWW/hadd/'
+    idir = '/eos/uscms/store/user/lpcbacon/dazsle/zprimebits-v15.01/skim/'
     odir = options.odir
     lumi = options.lumi
     isData = options.isData
     muonCR = options.muonCR
-
     
     legname = {'ggHbb': 'ggH(b#bar{b})',
                'Hbb': 'H(b#bar{b})',
@@ -72,7 +71,7 @@ def main(options,args,outputExists):
                'TTbar1Tau': 't#bar{t}+jets, 1#tau',        
                'TTbar0Lep': 't#bar{t}+jets, 0l',        
                'QCD': 'QCD',
-		       'data': 'JetHT data',
+               'data': 'JetHT data',
                'muon': 'SingleMuon data',
                'Phibb50': '#Phi(b#bar{b}), 50 GeV',
                'Phibb75': '#Phi(b#bar{b}), 75 GeV',
@@ -80,39 +79,18 @@ def main(options,args,outputExists):
                'Phibb250': '#Phi(b#bar{b}), 250 GeV'               
                }
 
-    if isData and muonCR:
-        legname['data'] = 'SingleMuon data'
-        
-    tfiles = {
-        'QCD_HT': [
-            #idir+'/QCD_HT100to200_13TeV_8X_1000pb_weighted.root',
-            #idir+'/QCD_HT200to300_13TeV_8X_1000pb_weighted.root',
-            idir+'/QCD_HT300to500_13TeV_8X_1000pb_weighted.root',
-            idir+'/QCD_HT500to700_13TeV_ext_8X_1000pb_weighted.root',
-            idir+'/QCD_HT1000to1500_13TeV_8X_1000pb_weighted.root',
-            idir+'/QCD_HT1500to2000_13TeV_ext_8X_1000pb_weighted.root',
-            idir+'/QCD_HT2000toInf_13TeV_ext_8X_1000pb_weighted.root'
-            ],
-        'QCD_HT700to1000': [
-            idir_hadd+'/QCD_HT700to1000_13TeV_8X_0.root',
-            idir_hadd+'/QCD_HT700to1000_13TeV_8X_1.root',
-            idir_hadd+'/QCD_HT700to1000_13TeV_8X_2.root',
-            idir_hadd+'/QCD_HT700to1000_13TeV_8X_3.root',
-            idir_hadd+'/QCD_HT700to1000_13TeV_8X_4.root',
-            idir_hadd+'/QCD_HT700to1000_13TeV_8X_5.root',
-            idir_hadd+'/QCD_HT700to1000_13TeV_8X_6.root'
-            ]
-        }
+    samplefiles   = open(os.path.expandvars("$ZPRIMEPLUSJET_BASE/analysis/ggH/samplefiles.json"),"r")
+    tfiles  = json.load(samplefiles)['controlPlotsGGH_2017']
 
     color = {'ggHbb': ROOT.kAzure+1,
              'Hbb': ROOT.kRed,
              'VHbb': ROOT.kTeal+1,
              'VBFHbb': ROOT.kBlue-10,
-		     'Phibb50': ROOT.kBlue-1,
+             'Phibb50': ROOT.kBlue-1,
              'Phibb75': ROOT.kAzure+1,
-		     'Phibb150': ROOT.kTeal+1,
-		     'Phibb250': ROOT.kMagenta+1,
-		     'ttHbb': ROOT.kBlue-1,
+             'Phibb150': ROOT.kTeal+1,
+             'Phibb250': ROOT.kMagenta+1,
+             'ttHbb': ROOT.kBlue-1,
              'Diboson': ROOT.kOrange,
              'SingleTop': ROOT.kRed-2,
              'DY':  ROOT.kRed+1,
@@ -123,19 +101,19 @@ def main(options,args,outputExists):
              'TTbar1Tau':  ROOT.kOrange+2,
              'TTbar0Lep':  ROOT.kGray,
              'QCD': ROOT.kBlue+2,
-		     'data':ROOT.kBlack,
-		     'muon':ROOT.kBlack
-            }
-
+             'data':ROOT.kBlack,
+             'muon':ROOT.kBlack
+             }
+    
     style = {'Hbb': 1,
              'ggHbb': 2,             
-		     'Phibb50': 2,
+             'Phibb50': 2,
              'Phibb75': 3,
-		     'Phibb150': 4,
-		     'Phibb250': 5,
+             'Phibb150': 4,
+             'Phibb250': 5,
              'VBFHbb': 3,
-		     'VHbb': 4,
-		     'ttHbb': 5,
+             'VHbb': 4,
+             'ttHbb': 5,
              'Diboson': 1,
              'SingleTop': 1,
              'DY': 1,
@@ -147,9 +125,9 @@ def main(options,args,outputExists):
              'TTbar0Lep': 1,
              'QCD': 1,
              'data': 1,
-		     'muon':1
-            }
-
+             'muon':1
+             }
+    
         
     canvases = []
     plots = ['h_msd_ak8_topR6_N2_pass','h_msd_ak8_topR6_N2_fail','h_msd_v_pt_ak8_topR6_N2_pass','h_msd_v_pt_ak8_topR6_N2_fail',
@@ -160,34 +138,11 @@ def main(options,args,outputExists):
     if not outputExists: 
         print "Backgrounds..."
         bkgSamples = {}    
-        bkgSamples['QCD_HT'] = sampleContainer('QCD_HT',tfiles['QCD_HT'], sf=1, 
-                                               DBTAGCUTMIN=DBTMIN, lumi=lumi, isData=False,
-                                               fillCA15=False, cutFormula = '1', minBranches =False, 
-                                               iSplit = options.iSplit, maxSplit = options.maxSplit, treeName = 'otree', 
-                                               doublebName = options.doublebName, doublebCut = options.doublebCut)
-        bkgSamples['QCD_HT700to1000'] = sampleContainer('QCD_HT700to1000',tfiles['QCD_HT700to1000'], sf=1, 
-                                                        DBTAGCUTMIN=DBTMIN, lumi=lumi*1000.*6831./1.55837e+07, isData=False,
-                                                        fillCA15=False, cutFormula = '1', minBranches=False, 
-                                                        iSplit = options.iSplit, maxSplit = options.maxSplit, treeName = 'Events',
-                                                        doublebName = options.doublebName, doublebCut = options.doublebCut)
-        #bkgSamples['TTbar1Mu']  = sampleContainer('TTbar1Mu',tfiles['TTbar'], 1, lumi, False, False, 'genMuFromW==1&&genEleFromW+genTauFromW==0')
-        #bkgSamples['TTbar1Ele']  = sampleContainer('TTbar1Ele',tfiles['TTbar'], 1, lumi, False, False, 'genEleFromW==1&&genMuFromW+genTauFromW==0')
-        #bkgSamples['TTbar1Tau']  = sampleContainer('TTbar1Tau',tfiles['TTbar'], 1, lumi, False, False, 'genTauFromW==1&&genEleFromW+genMuFromW==0')
-        #bkgSamples['TTbar0Lep']  = sampleContainer('TTbar0Lep',tfiles['TTbar'], 1, lumi, False, False, 'genMuFromW+genEleFromW+genTauFromW==0')
-        #bkgSamples['TTbar']  = sampleContainer('TTbar',tfiles['TTbar'], 1, lumi)
+        bkgSamples['QCD'] = normSampleContainer('QCD', tfiles['QCD'], 1, DBTMIN,lumi, False, False, '1', False, iSplit = options.iSplit, maxSplit = options.maxSplit,puOpt="default",doublebName=options.doublebName, doublebCut = options.doublebCut).addPlots(plots)
 
         ofile = ROOT.TFile.Open(odir+'/Ratios_1000pb_weighted_%s.root '%options.iSplit,'recreate')
 
-        hb = {}
-        for process, s in bkgSamples.iteritems():
-            hb[process] = {}
-            for plot in plots:
-                hb[process][plot] = getattr(s,plot)
-
-        hb['QCD'] = {}
-        for plot in plots:
-            hb['QCD'][plot] = hb['QCD_HT'][plot].Clone(hb['QCD_HT'][plot].GetName().replace('QCD_HT','QCD'))
-            hb['QCD'][plot].Add(hb['QCD_HT700to1000'][plot])
+        hb = bkgSamples
             
         makePlots(hb,style,odir,lumi,ofile,canvases)
         
@@ -196,21 +151,16 @@ def main(options,args,outputExists):
             for plot, h in hDict.iteritems():
                 h.Write()
         ofile.Close()
+
     else:
         
         ofile = ROOT.TFile.Open(odir+'/Ratios_1000pb_weighted.root','read')
 
-        
-        
         hb = {}
-        for process in ['QCD_HT','QCD_HT700to1000']:
+        for process in ['QCD']:
             hb[process] = {}
             for plot in plots:
                 hb[process][plot] = ofile.Get(plot.replace('h_','h_%s_'%process))
-        hb['QCD'] = {}
-        for plot in plots:
-            hb['QCD'][plot] = hb['QCD_HT'][plot].Clone(hb['QCD_HT'][plot].GetName().replace('QCD_HT','QCD'))
-            hb['QCD'][plot].Add(hb['QCD_HT700to1000'][plot])
                 
         hb['QCD']['h_msd_v_pt_ak8_topR6_N2_fail'].SetBinContent(23,1,0)
         hb['QCD']['h_msd_v_pt_ak8_topR6_N2_pass'].SetBinContent(23,1,0)
@@ -229,12 +179,6 @@ def main(options,args,outputExists):
         hb['QCD']['h_msd_v_pt_ak8_topR6_N2_fail'].SetBinContent(21,2,0)
         hb['QCD']['h_msd_v_pt_ak8_topR6_N2_pass'].SetBinContent(21,2,0)
 
-        # fix high bin
-        #ave = (hb['QCD']['h_msd_v_pt_ak8_topR6_N2_fail'].GetBinContent(12,4)+hb['QCD']['h_msd_v_pt_ak8_topR6_N2_fail'].GetBinContent(14,4))/2.
-        #hb['QCD']['h_msd_v_pt_ak8_topR6_N2_fail'].SetBinContent(13,4,ave)
-        #ave = (hb['QCD']['h_msd_v_pt_ak8_topR6_N2_fail'].GetBinError(12,4)+hb['QCD']['h_msd_v_pt_ak8_topR6_N2_fail'].GetBinError(14,4))/2.
-        #hb['QCD']['h_msd_v_pt_ak8_topR6_N2_fail'].SetBinError(13,4,ave)
-                
         hb['QCD']['h_msd_v_pt_ak8_topR6_N2_pass'].Scale(1./hb['QCD']['h_msd_v_pt_ak8_topR6_N2_pass'].Integral())
         hb['QCD']['h_msd_v_pt_ak8_topR6_N2_fail'].Scale(1./hb['QCD']['h_msd_v_pt_ak8_topR6_N2_fail'].Integral())
         hb['QCD']['h_msd_ak8_topR6_N2_pass'].Scale(1./hb['QCD']['h_msd_ak8_topR6_N2_pass'].Integral())
@@ -257,7 +201,7 @@ if __name__ == '__main__':
     parser.add_option('-m','--muonCR', action='store_true', dest='muonCR', default =False,help='for muon CR', metavar='muonCR')
     parser.add_option("--max-split", dest="maxSplit", default=1, type="int", help="max number of jobs", metavar="maxSplit")
     parser.add_option("--i-split"  , dest="iSplit", default=0, type="int", help="job number", metavar="iSplit")
-    parser.add_option("--double-b-name"  , dest="doublebName", default="AK8Puppijet0_deepdoublecsv", help="double-b name", metavar="doublebName")
+    parser.add_option("--double-b-name"  , dest="doublebName", default="AK8Puppijet0_deepdoubleb", help="double-b name", metavar="doublebName")
     parser.add_option("--double-b-cut"  , dest="doublebCut", default=0.9, type="float", help="double-b cut", metavar="doublebCut")
 
     (options, args) = parser.parse_args()
