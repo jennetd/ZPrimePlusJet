285947d83471484b0bc410d8db7848c2b7ce6cda
Javier Duarte <jduarte1@fnal.gov>
2019-02-22 09:48:27 -0600

    update comparison signals

--- analysis/comparisonSignals.py
+++ analysis/comparisonSignals.py
@@ -7,127 +7,122 @@ import math
 import sys
 import time
 import array
-
+import glob
+import os,json
 from plotHelpers import *
 from sampleContainer import *
-DBTAGMIN=-99
+from normSampleContainer import *
+DBTMIN=-99
 #
 
 ##############################################################################
 def main(options,args):
-    #idir = "/eos/uscms/store/user/lpchbb/ggHsample_V11/sklim-v0-28Oct/"
-    #odir = "plots_2016_10_31/"
-    idir = options.idir
-    idirData = 'root://cmseos.fnal.gov//eos/uscms/store/user/lpchbb/zprimebits-v12.05/'
-    odir = options.odir
     lumi = options.lumi
-    
-    legname = {#'ggHbb': 'ggH(b#bar{b}) N3LO + 0/1/2 merged',
-	       'ggHbbp': 'ggH(b#bar{b})',
+    i_split = options.iSplit
+    max_split = options.maxSplit
+    idir_1501skim = '/eos/uscms/store/user/lpcbacon/dazsle/zprimebits-v15.01/skim/'
+        
+    legname = {
+	       'ggHbb': 'ggH(b#bar{b})',
                'VBFHbb':'VBF H(b#bar{b})',
 	       'ZHbb': ' Z(q#bar{q})H(b#bar{b})',
 	       'ZnnHbb': ' Z(#nu#nu)H(b#bar{b})',
 	       'WHbb': 'W(q#bar{q})H(b#bar{b})',
-	       'tthbb': 'ttH(b#bar{b})',	   	
-	       'Phibb': ' Phi(125)(b#bar{b})'}
-
-        
-    tfiles = {#'ggHbb': [idirData + '/GluGluHToBB_M125_13TeV_powheg_pythia8_CKKW_1000pb_weighted.root']
-	       'ggHbbp': [idir+'/GluGluHToBB_M125_13TeV_powheg_pythia8_all_1000pb_weighted.root'],
-               'VBFHbb': [idir+'/VBFHToBB_M_125_13TeV_powheg_pythia8_weightfix_all_1000pb_weighted.root'],
-               'ZHbb': [idir+'/ZH_HToBB_ZToQQ_M125_13TeV_powheg_pythia8_1000pb_weighted.root',
-			idir+'/ggZH_HToBB_ZToQQ_M125_13TeV_powheg_pythia8_1000pb_weighted.root'],
-               'WHbb' : [idir+'/WminusH_HToBB_WToQQ_M125_13TeV_powheg_pythia8_1000pb_weighted.root',
-                        idir+'/WplusH_HToBB_WToQQ_M125_13TeV_powheg_pythia8_1000pb_weighted.root'],
-               'tthbb' : [idir+'/ttHTobb_M125_13TeV_powheg_pythia8_1000pb_weighted.root'],
-	       'ZnnHbb': [idir+'/ggZH_HToBB_ZToNuNu_M125_13TeV_powheg_pythia8_1000pb_weighted.root',
-			  idir+'/ZH_HToBB_ZToNuNu_M125_13TeV_powheg_pythia8_ext_1000pb_weighted.root'],
-	       'Phibb':[idir+'/Spin0_ggPhi12j_g1_125_Scalar_13TeV_madgraph_1000pb_weighted.root']
+	       'ttHbb': 'ttH(b#bar{b})'
                }
-
-    color = {#'ggHbb': ROOT.kBlack,
-	     'ggHbbp': ROOT.kBlue+2,
+    
+    tfiles = {'ggHbb_amc' :  { 'GluGluHToBB_M125_13TeV_amcatnloFXFX_pythia8':                glob.glob(idir_1501skim+'GluGluHToBB_M125_13TeV_amcatnloFXFX_pythia8_*.root')},
+          'ggHbb_amcHpT250' :  { 'GluGluHToBB_M125_LHEHpT_250_Inf_13TeV_amcatnloFXFX_pythia8' : glob.glob(idir_1501skim+'GluGluHToBB_M125_LHEHpT_250_Inf_13TeV_amcatnloFXFX_pythia8_*.root')},
+              'ggHbb'     :          { 'GluGluHToBB_M125_13TeV_powheg_pythia8':                      glob.glob(idir_1501skim+'/GluGluHToBB_M125_13TeV_powheg_pythia8_*.root')},
+              'VBFHbb'    :          { 'VBFHToBB_M_125_13TeV_powheg_pythia8_weightfix':              glob.glob(idir_1501skim+'/VBFHToBB_M_125_13TeV_powheg_pythia8_weightfix_*.root')},
+              'ZnnHbb'      :          { 
+            'ZH_HToBB_ZToNuNu_M125_13TeV_powheg_pythia8':            glob.glob(idir_1501skim+'/ZH_HToBB_ZToQQ_M125_13TeV_powheg_pythia8_*.root'),
+            },
+              'ZHbb' : {
+            'ZH_HToBB_ZToQQ_M125_13TeV_powheg_pythia8':              glob.glob(idir_1501skim+'/ZH_HToBB_ZToQQ_M125_13TeV_powheg_pythia8_*.root'),
+            'ggZH_HToBB_ZToQQ_M125_13TeV_powheg_pythia8':            glob.glob(idir_1501skim+'/ggZH_HToBB_ZToQQ_M125_13TeV_powheg_pythia8_*.root'),
+            },
+              'WHbb' : {
+            'WminusH_HToBB_WToQQ_M125_13TeV_powheg_pythia8':         glob.glob(idir_1501skim+'/WminusH_HToBB_WToQQ_M125_13TeV_powheg_pythia8_*.root'),
+            'WplusH_HToBB_WToQQ_M125_13TeV_powheg_pythia8':          glob.glob(idir_1501skim+'/WplusH_HToBB_WToQQ_M125_13TeV_powheg_pythia8_*.root'),
+            },
+              'ttHbb'    :          { 'ttHTobb_M125_TuneCP5_13TeV_powheg_pythia8':           glob.glob(idir_1501skim+'/ttHTobb_M125_TuneCP5_13TeV_powheg_pythia8_*.root')},
+              }
+    
+    color = {
+             'ggHbb': ROOT.kBlue+2,
              'VBFHbb': ROOT.kAzure+3,
-	     'ZnnHbb': ROOT.kPink+5,
+             'ZnnHbb': ROOT.kPink+5,
 	     'ZHbb': ROOT.kPink+1,
 	     'WHbb': ROOT.kAzure+1,	
-	     'tthbb': ROOT.kOrange+1,
-	     'Phibb':ROOT.kRed-2
+	     'ttHbb': ROOT.kOrange+1,
                }
 
-    style = {#'ggHbb': 1,
-	     'ggHbbp': 1,
+    style = {
+	     'ggHbb': 1,
              'VBFHbb': 2,
 	     'ZHbb': 1,
 	     'WHbb':1,
              'ZnnHbb': 1,
-	     'tthbb':1,		
-	     'Phibb':2
+	     'ttHbb':1,		
                }
-        
-    print "Signals... "
-    sigSamples = {}
-    #sigSamples['ggHbb']  = sampleContainer('ggHbb',tfiles['ggHbb']  , 1, lumi ) 
-    sigSamples['ggHbbp']  = sampleContainer('ggHbbp',tfiles['ggHbbp']  , 1, DBTAGMIN,lumi)
-    sigSamples['VBFHbb'] = sampleContainer('VBFHbb',tfiles['VBFHbb'], 1, DBTAGMIN,lumi) 
-    sigSamples['ZHbb'] = sampleContainer('ZHbb',tfiles['ZHbb'], 1, DBTAGMIN,lumi ) 	
-    sigSamples['WHbb'] = sampleContainer('WHbb',tfiles['WHbb'], 1, DBTAGMIN,lumi )
-    sigSamples['tthbb'] = sampleContainer('tthbb',tfiles['tthbb'], 1, DBTAGMIN,lumi )	
-    sigSamples['ZnnHbb'] = sampleContainer('ZnnHbb',tfiles['ZnnHbb'], 1, DBTAGMIN,lumi )
-    #sigSamples['Phibb'] = sampleContainer('Phibb',tfiles['Phibb'], 1, lumi*0.035)   
 
+    plots = [
+        'h_n_ak4'           ,
+        'h_met'             ,
+        'h_pt_ak8'          ,
+        'h_pt_ak8_dbtagCut' ,
+        'h_msd_ak8'         ,
+        'h_msd_ak8_dbtagCut',
+        'h_msd_ak8_t21ddtCut'  ,
+        'h_msd_ak8_N2Cut'   ,
+        'h_dbtag_ak8'       ,
+        'h_t21ddt_ak8'      ,
+        'h_t32_ak8'         ,
+        'h_t32_ak8_t21ddtCut'  ,
+        'h_n2b1sd_ak8'      ,
+        'h_n2b1sdddt_ak8'   ,
+        'h_pt_bbleading' ,
+        'h_bb_bbleading' ,
+        'h_msd_bbleading',
+        'h_msd_ak8_inc',
+        'h_msd_ak8_raw',
+        'h_msd_ak8_topR6_N2_pass',
+        'h_msd_ak8_topR6_N2_fail'
+        ]
+        
+    print("Signals... ")
+    sigSamples = {}
+    sigSamples['ggHbb']  = normSampleContainer('ggHbb',tfiles['ggHbb']  , 1, DBTMIN,lumi,False,False,'1',False, iSplit = i_split, maxSplit = max_split,puOpt='default',doublebName='AK8Puppijet0_deepdoubleb').addPlots(plots)
+    sigSamples['ggHbb_amc']  = normSampleContainer('ggHbb_amc',tfiles['ggHbb_amc']  , 1, DBTMIN,lumi,False,False,'1',False, iSplit = i_split, maxSplit = max_split,puOpt='default',doublebName='AK8Puppijet0_deepdoubleb').addPlots(plots)
+    sigSamples['VBFHbb']  = normSampleContainer('VBFHbb',tfiles['VBFHbb']  , 1, DBTMIN,lumi,False,False,'1',False, iSplit = i_split, maxSplit = max_split,puOpt='default',doublebName='AK8Puppijet0_deepdoubleb').addPlots(plots)
+    sigSamples['ZHbb']  = normSampleContainer('ZHbb',tfiles['ZHbb']  , 1, DBTMIN,lumi,False,False,'1',False, iSplit = i_split, maxSplit = max_split,puOpt='default',doublebName='AK8Puppijet0_deepdoubleb').addPlots(plots)
+    sigSamples['WHbb']  = normSampleContainer('WHbb',tfiles['WHbb']  , 1, DBTMIN,lumi,False,False,'1',False, iSplit = i_split, maxSplit = max_split,puOpt='default',doublebName='AK8Puppijet0_deepdoubleb').addPlots(plots)
+    sigSamples['ttHbb']  = normSampleContainer('ttHbb',tfiles['ttHbb']  , 1, DBTMIN,lumi,False,False,'1',False, iSplit = i_split, maxSplit = max_split,puOpt='default',doublebName='AK8Puppijet0_deepdoubleb').addPlots(plots)
+    sigSamples['ZnnHbb']  = normSampleContainer('ZnnHbb',tfiles['ZnnHbb']  , 1, DBTMIN,lumi,False,False,'1',False, iSplit = i_split, maxSplit = max_split,puOpt='default',doublebName='AK8Puppijet0_deepdoubleb').addPlots(plots)
 
     ofile = ROOT.TFile.Open(odir+'/Plots_1000pb_weighted.root','recreate')
 
-
-
-    plots = [
-'h_n_ak4'           ,
-'h_met'             ,
-'h_pt_ak8'          ,
-'h_pt_ak8_dbtagCut' ,
-'h_msd_ak8'         ,
-'h_msd_ak8_dbtagCut',
-'h_msd_ak8_t21ddtCut'  ,
-'h_msd_ak8_N2Cut'   ,
-'h_dbtag_ak8'       ,
-'h_t21ddt_ak8'      ,
-'h_t32_ak8'         ,
-'h_t32_ak8_t21ddtCut'  ,
-'h_n2b1sd_ak8'      ,
-'h_n2b1sdddt_ak8'   ,
-'h_pt_bbleading' ,
-'h_bb_bbleading' ,
-'h_msd_bbleading',
-'h_msd_ak8_inc',
-'h_msd_ak8_raw',
-'h_msd_ak8_topR6_N2_pass',
-'h_msd_ak8_topR6_N2_fail'
-]
-#'h_pt_ak8','h_msd_ak8','h_dbtag_ak8','h_n_ak4','h_n_ak4_dR0p8','h_pt_ak8_dbtagCut','h_msd_ak8_dbtagCut','h_t21_ak8','h_t32_ak8','h_msd_ak8_t21ddtCut','h_msd_ak8_N2Cut','h_met']
+    hs = sigSamples
     for plot in plots:
-        hs = {}
-        for process, s in sigSamples.iteritems():
-            hs[process] = getattr(s,plot)
         c = makeCanvasComparison(hs,legname,color,style,plot.replace('h_','signalcomparison_'),odir,lumi)
-
         ofile.cd()
         for process, h in hs.iteritems():
-            h.Write()
-
-        
+            h.Write()        
         c.Write()
-	
-
 
 ##----##----##----##----##----##----##
 if __name__ == '__main__':
 
     parser = OptionParser()
     parser.add_option('-b', action='store_true', dest='noX', default=False, help='no X11 windows')
-    parser.add_option("--lumi", dest="lumi", type=float,default = 35.9,help="luminosity", metavar="lumi")
+    parser.add_option("--lumi", dest="lumi", type=float,default = 41,help="luminosity", metavar="lumi")
     parser.add_option('-i','--idir', dest='idir', default = 'data/',help='directory with data', metavar='idir')
     parser.add_option('-o','--odir', dest='odir', default = 'plots/',help='directory to write plots', metavar='odir')
+    parser.add_option("--max-split", dest="maxSplit", default=1, type="int", help="max number of jobs", metavar="maxSplit")
+    parser.add_option("--i-split"  , dest="iSplit", default=0, type="int", help="job number", metavar="iSplit")
+    parser.add_option("--double-b-name"  , dest="doublebName", default="AK8Puppijet0_deepdoubleb", help="double-b name", metavar="doublebName")
+    parser.add_option("--double-b-cut"  , dest="doublebCut", default=0.89, type="float", help="double-b cut", metavar="doublebCut")
 
     (options, args) = parser.parse_args()
 
