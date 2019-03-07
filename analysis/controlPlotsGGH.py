6ec32816d81e3f1a4d93ffa3e027710a8f756ff9
Martin <ka_hei_martin_kwok@brown.edu>
2019-03-07 10:04:15 -0600

    2017F use nodupl skim

--- analysis/controlPlotsGGH.py
+++ analysis/controlPlotsGGH.py
@@ -370,7 +370,8 @@ def get2017files():
                              idir_1501skim + 'JetHTRun2017C_17Nov2017_v1_*.root',
                              idir_1501skim + 'JetHTRun2017D_17Nov2017_v1_*.root',
                              idir_1501skim + 'JetHTRun2017E_17Nov2017_v1_*.root',
-                             idir_1501skim + 'JetHTRun2017F_17Nov2017_v1_*.root'
+                             #idir_1501skim + 'JetHTRun2017F_17Nov2017_v1_*.root'
+                             idir_1501skim + 'JetHTRun2017F_17Nov2017_v1_*.rootnodupl.root',
                       ],
               'muon': [
                        idir_1501skim+'/SingleMuonRun2017B_17Nov2017_v1_*.root',
