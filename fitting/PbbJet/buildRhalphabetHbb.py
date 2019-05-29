d317a9eb82c34bc1d5019c00eb8c17d2f2cfbbfe
Martin <ka_hei_martin_kwok@brown.edu>
2019-05-29 02:48:53 -0500

    update bbSF 2016 err

--- fitting/PbbJet/buildRhalphabetHbb.py
+++ fitting/PbbJet/buildRhalphabetHbb.py
@@ -48,7 +48,7 @@ SF2016={
             'm_mc'      : 82.548,           'm_mc_err'  : 0.191,
             's_data'    : 8.701,            's_data_err': 0.433,
             's_mc'      : 8.027,            's_mc_err'  : 0.607,
-            'BB_SF'     : 0.68,             'BB_SF_ERR' : 0.2,
+            'BB_SF'     : 0.68,             'BB_SF_ERR' : 0.15,
             'V_SF'      : 0.993,            'V_SF_ERR'  : 0.043,
         }
 
